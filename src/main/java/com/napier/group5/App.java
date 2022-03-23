98bcc562da92cd78bc7568173a0ba1bb1a6ac2d1
Sai-Bhone-Myat-Naing <40501775@live.napier.ac.uk>
2022-03-23 22:23:51 +0630

    Add Country report.

--- src/main/java/com/napier/group5/App.java
+++ src/main/java/com/napier/group5/App.java
@@ -1,62 +1,108 @@
 package com.napier.group5;
 
 import java.sql.*;
+import java.util.ArrayList;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        try
-        {
+public class App {
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    public static void main(String[] args) throws SQLException {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect("db:3306", 30000);
+        // Disconnect from database
+        System.out.println("In main");
+        ArrayList<Country> contries= a.getCountryPopLargesttoSmallest();
+        a.display(contries);
+        System.out.println("*****************************************************\nContinent\n");
+        ArrayList<Country> contries2= a.getCountryPopbyContinent("Asia");
+        a.display(contries2);
+        a.disconnect();
+    }
+
+    /**
+     * Connect to the MySQL database.
+
+     */
+    public void connect(String conString, int delay) {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
-        for (int i = 0; i < retries; ++i)
-        {
+        int retries = 10;
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(delay);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                //Added allowPublicKeyRetrieval=true to get Integration Tests to work. Possibly due to accessing from another class?
+                con = DriverManager.getConnection("jdbc:mysql://" + conString + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+            } catch (SQLException sqle) {
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
-
-        if (con != null)
-        {
-            try
-            {
+    }
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
     }
+    public ArrayList<Country> getCountryPopLargesttoSmallest() throws SQLException {
+        String sql ="select Name,Continent,Region, Capital, Population from country order by Population desc";
+        PreparedStatement pstmt =con.prepareStatement(sql);
+        ArrayList<Country> countries = new ArrayList<Country>();
+        ResultSet rset =pstmt.executeQuery();
+        //String name, String continent, String region, String capital, float population
+        while(rset.next())
+        {Country  c = new Country(rset.getString(1),rset.getString(2),rset.getString(3),rset.getString(4),rset.getFloat(5));
+            countries.add(c);
+        }
+        return countries;
+    }
+
+    public ArrayList<Country> getCountryPopbyContinent(String contn) throws SQLException {
+        String sql ="select Name,Continent,Region, Capital, Population from country where Continent=? order by Population desc";
+        PreparedStatement pstmt =con.prepareStatement(sql);
+        pstmt.setString(1,contn);
+        ArrayList<Country> countries = new ArrayList<Country>();
+        ResultSet rset =pstmt.executeQuery();
+        //String name, String continent, String region, String capital, float population
+        while(rset.next())
+        {Country  c = new Country(rset.getString(1),rset.getString(2),rset.getString(3),rset.getString(4),rset.getFloat(5));
+            countries.add(c);
+        }
+        return countries;
+    }
+
+    public void display(ArrayList<Country> conts)
+    {
+        for(Country c: conts)
+        {
+            System.out.println(c.getName()+"\t"+c.getContinent()+"\t"+c.getRegion()+"\t"+c.getCapital()+"\t"+c.getPopulation());
+        }
+    }
+
 }
