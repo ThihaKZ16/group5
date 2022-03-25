191c9d2dfe11b927b688bf42b728854e75a6008e
ThihaKZ16 <40501778@live.napier.ac.uk>
2022-03-25 14:09:31 +0630

    Updated App java of country-report for Codereview2.

--- src/main/java/com/napier/group5/App.java
+++ src/main/java/com/napier/group5/App.java
@@ -18,26 +18,26 @@ public class App {
         a.connect("db:3306", 30000);
 
         System.out.println("All the countries in the world organised by largest population to smallest.");
-        ArrayList<Country> contries= a.getCountryPopLargesttoSmallest();
-        a.display(contries);
+        ArrayList<Country> countries= a.getCountryPopLargesttoSmallest();
+        a.display(countries);
 
         System.out.println("All the countries in the world organised by largest population to smallest.");
-        ArrayList<Country> contries2= a.getCountryPopbyContinent("Asia");
-        a.display(contries2);
+        ArrayList<Country> countries2= a.getCountryPopbyContinent("Asia");
+        a.display(countries2);
 
         System.out.println("All the countries in a region organised by largest population to smallest.");
-        ArrayList<Country> contries3= a.getCountryPopbyRegion("Caribbean");
-        a.display(contries3);
+        ArrayList<Country> countries3= a.getCountryPopbyRegion("Caribbean");
+        a.display(countries3);
 
         // Disconnect from database
         a.disconnect();
-
     }
 
     /**
      * Connect to the MySQL database.
 
      */
+
     public void connect(String conString, int delay) {
         try {
             // Load Database driver
@@ -65,9 +65,11 @@ public class App {
             }
         }
     }
+
     /**
      * Disconnect from the MySQL database.
      */
+
     public void disconnect() {
         if (con != null) {
             try {
@@ -78,12 +80,14 @@ public class App {
             }
         }
     }
+
+    //Extract the output of all the countries in the world organised by largest population to smallest.
     public ArrayList<Country> getCountryPopLargesttoSmallest() throws SQLException {
         String sql ="select Code, Name,Continent,Region, Capital, Population from country order by Population desc";
         PreparedStatement pstmt =con.prepareStatement(sql);
         ArrayList<Country> countries = new ArrayList<Country>();
         ResultSet rset =pstmt.executeQuery();
-        //String name, String continent, String region, String capital, float population
+        //String code, String name, String continent, String region, String capital, float population
         while(rset.next())
         {Country  c = new Country(rset.getString(1),rset.getString(2),rset.getString(3),rset.getString(4),rset.getString(5),rset.getFloat(6));
             countries.add(c);
@@ -91,32 +95,37 @@ public class App {
         return countries;
     }
 
+    //Extract the output of countries in a continent organised by largest population to smallest.
     public ArrayList<Country> getCountryPopbyContinent(String contn) throws SQLException {
         String sql ="select Code, Name,Continent,Region, Capital, Population from country where Continent=? order by Population desc";
         PreparedStatement pstmt =con.prepareStatement(sql);
         pstmt.setString(1,contn);
         ArrayList<Country> countries2 = new ArrayList<Country>();
         ResultSet rset =pstmt.executeQuery();
-        //String name, String continent, String region, String capital, float population
+        //String code, String name, String continent, String region, String capital, float population
         while(rset.next())
         {Country  c = new Country(rset.getString(1),rset.getString(2),rset.getString(3),rset.getString(4), rset.getString(5),rset.getFloat(6));
             countries2.add(c);
         }
         return countries2;
     }
+
+    //Extract the output of countries in a region organised by largest population to smallest.
     public ArrayList<Country> getCountryPopbyRegion(String contn) throws SQLException {
         String sql ="select Code,Name,Continent,Region,Capital,Population from country where Region=? order by Population desc";
         PreparedStatement pstmt =con.prepareStatement(sql);
         pstmt.setString(1,contn);
         ArrayList<Country> countries3 = new ArrayList<Country>();
         ResultSet rset =pstmt.executeQuery();
-        //String name, String continent, String region, String capital, float population
+        //String code, String name, String continent, String region, String capital, float population
         while(rset.next())
         {Country  c = new Country(rset.getString(1),rset.getString(2),rset.getString(3),rset.getString(4), rset.getString(5),rset.getFloat(6));
             countries3.add(c);
         }
         return countries3;
     }
+
+    //Display function
     public void display(ArrayList<Country> conts)
     {
         for(Country c: conts)
