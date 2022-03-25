fa1b2776941072ff76dca348b3b85043756a776e
ThihaKZ16 <40501778@live.napier.ac.uk>
2022-03-25 15:41:37 +0630

    Updated App java for country-report for Codereview2.

--- src/main/java/com/napier/group5/App.java
+++ src/main/java/com/napier/group5/App.java
@@ -21,7 +21,7 @@ public class App {
         ArrayList<Country> countries= a.getCountryPopLargesttoSmallest();
         a.display(countries);
 
-        System.out.println("All the countries in the world organised by largest population to smallest.");
+        System.out.println("All the countries in the continent organised by largest population to smallest.");
         ArrayList<Country> countries2= a.getCountryPopbyContinent("Asia");
         a.display(countries2);
 
