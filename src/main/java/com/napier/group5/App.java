210debe5b2905668c07ba3903861f3d50dbcae0a
SithuMinZin-40501790 <101235752+SithuMinZin-40501790@users.noreply.github.com>
2022-03-27 14:58:56 +0630

    Update App.java

--- src/main/java/com/napier/group5/App.java
+++ src/main/java/com/napier/group5/App.java
@@ -17,11 +17,11 @@ public class App {
         // Connect to database
         a.connect("db:3306", 30000);
 
-        System.out.println("All the countries in the world organised by largest population to smallest.");
+        System.out.println("All the countries in this WORLD  organised by largest population to smallest.");
         ArrayList<Country> countries= a.getCountryPopLargesttoSmallest();
         a.display(countries);
 
-        System.out.println("All the countries in the continent organised by largest population to smallest.");
+        System.out.println("All the countries in a continent organised by largest population to smallest.");
         ArrayList<Country> countries2= a.getCountryPopbyContinent("Asia");
         a.display(countries2);
 
