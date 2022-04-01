7906fc799e6d068413c7819e674dc9e6f2bfd710
40501769 <40501769@live.napier.ac.uk>
2022-04-01 15:15:30 +0630

    Integration test for country-report.

--- src/test/java/com/napier/group5/CountryIntegrationTest.java
+++ src/test/java/com/napier/group5/CountryIntegrationTest.java
@@ -60,5 +60,68 @@ public class CountryIntegrationTest
         assertEquals(1.26714E8,country.getPopulation());
     }
 
+    @Test
+    void getcountriesinaregion() throws SQLException {
+        ArrayList<Country> countries2 = app.getCountryPopbyRegion("Caribbean");
+        Country country=null;boolean flag= false;
+        for (Country c:countries2){
+            //1134,'South Dum Dum','IND','West Bengali',232811
+            if (c.getCode().equals("TCA"))
+            {
+                country = c;flag = true;
+                break;
+            }
+        }
+//        assertEquals("Japan",country.getConame());
+        assertEquals(17000.0,country.getPopulation());
+    }
+
+    @Test
+    void gettopcountriesintheworld() throws SQLException {
+        ArrayList<Country> countries3 = app.gettopCountryPopLargesttoSmallest(3);
+        Country country=null;boolean flag= false;
+        for (Country c:countries3){
+            //1134,'South Dum Dum','IND','West Bengali',232811
+            if (c.getCode().equals("IND"))
+            {
+                country = c;flag = true;
+                break;
+            }
+        }
+//        assertEquals("Japan",country.getConame());
+        assertEquals(1.013662016E9,country.getPopulation());
+    }
+
+    @Test
+    void gettopcountriesinthecontinent() throws SQLException {
+        ArrayList<Country> countries4 = app.gettopCountryPopbyContinent("Asia", 2);
+        Country country=null;boolean flag= false;
+        for (Country c:countries4){
+            //1134,'South Dum Dum','IND','West Bengali',232811
+            if (c.getCode().equals("CHN"))
+            {
+                country = c;flag = true;
+                break;
+            }
+        }
+//        assertEquals("Japan",country.getConame());
+        assertEquals(1.277558016E9,country.getPopulation());
+    }
+
+    @Test
+    void gettopcountriesintheregion() throws SQLException {
+        ArrayList<Country> countries5 = app.gettopCountryPopbyRegion("Caribbean", 2);
+        Country country=null;boolean flag= false;
+        for (Country c:countries5){
+            //1134,'South Dum Dum','IND','West Bengali',232811
+            if (c.getCode().equals("DOM"))
+            {
+                country = c;flag = true;
+                break;
+            }
+        }
+//        assertEquals("Japan",country.getConame());
+        assertEquals(8495000.0,country.getPopulation());
+    }
 
 }
