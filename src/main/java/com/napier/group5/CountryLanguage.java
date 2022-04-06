e4f8a8802fc745d235f3a680ffaea03cde9936f4
Sai-Bhone-Myat-Naing <40501775@live.napier.ac.uk>
2022-04-06 11:08:31 +0630

    Add CountryLanguage java file for variable.

---
+++ src/main/java/com/napier/group5/CountryLanguage.java
@@ -0,0 +1,33 @@
+package com.napier.group5;
+
+public class CountryLanguage {
+    public String code;
+    public String language;
+    public Float percentage;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(Float percentage) {
+        this.percentage = percentage;
+    }
+
+
+}
