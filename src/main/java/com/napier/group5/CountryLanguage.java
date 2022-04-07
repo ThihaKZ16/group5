b0388f86fda3831774fac34a218b880df151a958
Sai-Bhone-Myat-Naing <40501775@live.napier.ac.uk>
2022-04-07 14:16:42 +0630

    Add suitable comments in code.

--- src/main/java/com/napier/group5/CountryLanguage.java
+++ src/main/java/com/napier/group5/CountryLanguage.java
@@ -1,47 +1,14 @@
 package com.napier.group5;
-
+//Build countrylanguage class
 public class CountryLanguage {
 
 
-
+/*Declaration for the Country Language variable*/
     public String countrycode;
     public String language;
     public Float percentage;
     public String coname;
     public Float population;
-
-    public Float getChnpopulation() {
-        return chnpopulation;
-    }
-
-    public void setChnpopulation(Float chnpopulation) {
-        this.chnpopulation = chnpopulation;
-    }
-
-    public Float getArapopulation() {
-        return arapopulation;
-    }
-
-    public void setArapopulation(Float arapopulation) {
-        this.arapopulation = arapopulation;
-    }
-
-    public Float getHinpopulation() {
-        return hinpopulation;
-    }
-
-    public void setHinpopulation(Float hinpopulation) {
-        this.hinpopulation = hinpopulation;
-    }
-
-    public Float getSpanpopulation() {
-        return spanpopulation;
-    }
-
-    public void setSpanpopulation(Float spanpopulation) {
-        this.spanpopulation = spanpopulation;
-    }
-
     public Float chnpopulation;
     public Float arapopulation;
     public Float hinpopulation;
@@ -57,130 +24,170 @@ public class CountryLanguage {
     public Float noperhinditotalpopulation;
     public Float noperspanishtotalpopulation;
 
-
-    public Float getNoperengtotalpopulation() {
-        return noperengtotalpopulation;
+    /*Getter and setter function Chinapopulation*/
+    public Float getChnpopulation() {
+        return chnpopulation;
+    }
+    /*Getter and setter function Chinapopulation*/
+    public void setChnpopulation(Float chnpopulation) {
+        this.chnpopulation = chnpopulation;
     }
 
-    public void setNoperengtotalpopulation(Float noperengtotalpopulation) {
-        this.noperengtotalpopulation = noperengtotalpopulation;
-    }
-
+    /*Getter and setter function NopercentageChinapopulation*/
     public Float getNoperchinatotalpopulation() {
         return noperchinatotalpopulation;
     }
-
+    /*Getter and setter function NopercentageChinapopulation*/
     public void setNoperchinatotalpopulation(Float noperchinatotalpopulation) {
         this.noperchinatotalpopulation = noperchinatotalpopulation;
     }
-
-    public Float getNoperarabictotalpopulation() {
-        return noperarabictotalpopulation;
-    }
-
-    public void setNoperarabictotalpopulation(Float noperarabictotalpopulation) {
-        this.noperarabictotalpopulation = noperarabictotalpopulation;
-    }
-
-    public Float getNoperhinditotalpopulation() {
-        return noperhinditotalpopulation;
-    }
-
-    public void setNoperhinditotalpopulation(Float noperhinditotalpopulation) {
-        this.noperhinditotalpopulation = noperhinditotalpopulation;
-    }
-
-    public Float getNoperspanishtotalpopulation() {
-        return noperspanishtotalpopulation;
-    }
-
-    public void setNoperspanishtotalpopulation(Float noperspanishtotalpopulation) {
-        this.noperspanishtotalpopulation = noperspanishtotalpopulation;
-    }
-
-
-
+    /*Getter and setter function TotalChinapopulation*/
     public Float getChinesetotalpopulation() {
         return chinesetotalpopulation;
     }
-
+    /*Getter and setter function Chinapopulation*/
     public void setChinesetotalpopulation(Float chinesetotalpopulation) {
         this.chinesetotalpopulation = chinesetotalpopulation;
     }
 
-    public Float getHinditotalpopulation() {
-        return hinditotalpopulation;
+    /*Getter and setter function Arabicpopulation*/
+    public Float getArapopulation() {
+        return arapopulation;
     }
-
-    public void setHinditotalpopulation(Float hinditotalpopulation) {
-        this.hinditotalpopulation = hinditotalpopulation;
+    /*Getter and setter function Arabicpopulation*/
+    public void setArapopulation(Float arapopulation) {
+        this.arapopulation = arapopulation;
     }
-
-    public Float getSpanishtotalpopulation() {
-        return spanishtotalpopulation;
+    /*Getter and setter function NopercentageArabicpopulation*/
+    public Float getNoperarabictotalpopulation() {
+        return noperarabictotalpopulation;
     }
-
-    public void setSpanishtotalpopulation(Float spanishtotalpopulation) {
-        this.spanishtotalpopulation = spanishtotalpopulation;
+    /*Getter and setter function NopercentageArabicpopulation*/
+    public void setNoperarabictotalpopulation(Float noperarabictotalpopulation) {
+        this.noperarabictotalpopulation = noperarabictotalpopulation;
     }
-
+    /*Getter and setter function TotalArabicpopulation*/
     public Float getArabictotalpopulation() {
         return arabictotalpopulation;
     }
-
+    /*Getter and setter function TotalArabicpopulation*/
     public void setArabictotalpopulation(Float arabictotalpopulation) {
         this.arabictotalpopulation = arabictotalpopulation;
     }
 
+    /*Getter and setter function Hindipopulation*/
+    public Float getHinpopulation() {
+        return hinpopulation;
+    }
+    /*Getter and setter function Hindipopulation*/
+    public void setHinpopulation(Float hinpopulation) {
+        this.hinpopulation = hinpopulation;
+    }
+
+    /*Getter and setter function NopercentageHindipopulation*/
+    public Float getNoperhinditotalpopulation() {
+        return noperhinditotalpopulation;
+    }
+
+    /*Getter and setter function NopercentageHindipopulation*/
+    public void setNoperhinditotalpopulation(Float noperhinditotalpopulation) {
+        this.noperhinditotalpopulation = noperhinditotalpopulation;
+    }
+
+    /*Getter and setter function TotalHindipopulation*/
+    public Float getHinditotalpopulation() {
+        return hinditotalpopulation;
+    }
+
+    /*Getter and setter function TotalHindipopulation*/
+    public void setHinditotalpopulation(Float hinditotalpopulation) {
+        this.hinditotalpopulation = hinditotalpopulation;
+    }
+
+    /*Getter and setter function Spanishpopulation*/
+    public Float getSpanpopulation() {
+        return spanpopulation;
+    }
+    /*Getter and setter function Spanishpopulation*/
+    public void setSpanpopulation(Float spanpopulation) {
+        this.spanpopulation = spanpopulation;
+    }
+    /*Getter and setter function NopercentageSpanishpopulation*/
+    public void setNoperspanishtotalpopulation(Float noperspanishtotalpopulation) {
+        this.noperspanishtotalpopulation = noperspanishtotalpopulation;
+    }
+    /*Getter and setter function TotalSpanishpopulation*/
+    public Float getSpanishtotalpopulation() {
+        return spanishtotalpopulation;
+    }
+    /*Getter and setter function TotalSpanishpopulation*/
+    public void setSpanishtotalpopulation(Float spanishtotalpopulation) {
+        this.spanishtotalpopulation = spanishtotalpopulation;
+    }
+
+    /*Getter and setter function NopercentageSpanishpopulation*/
+    public Float getNoperspanishtotalpopulation() {
+        return noperspanishtotalpopulation;
+    }
 
 
+
+
+    /*Getter and setter function TotalEnglishpopulation*/
     public Float getTotalpopulation() {
         return totalpopulation;
     }
-
+    /*Getter and setter function TotalEnglishpopulation*/
     public void setTotalpopulation(Float totalpopulation) {
         this.totalpopulation = totalpopulation;
     }
-
-
-
+    /*Getter and setter function Englishpopulation*/
     public Float getPopulation() {
         return population;
     }
-
+    /*Getter and setter function Englishpopulation*/
     public void setPopulation(Float population) {
         this.population = population;
     }
+    /*Getter and setter function NopercentageEnglishpopulation*/
+    public Float getNoperengtotalpopulation() {
+        return noperengtotalpopulation;
+    }
+    /*Getter and setter function NopercentageEnglishpopulation*/
+    public void setNoperengtotalpopulation(Float noperengtotalpopulation) {
+        this.noperengtotalpopulation = noperengtotalpopulation;
+    }
 
-
-
+    /*Getter and setter function for countrylanguage database variables(country code)*/
     public String getCountryCode() {
         return countrycode;
     }
-
+    /*Getter and setter function for countrylanguage database variables(country code)*/
     public void setCode(String code) {
         this.countrycode = countrycode;
     }
-
+    /*Getter and setter function for countrylanguage database variables(country language)*/
     public String getLanguage() {
         return language;
     }
-
+    /*Getter and setter function for countrylanguage database variables(country language)*/
     public void setLanguage(String language) {
         this.language = language;
     }
+    /*Getter and setter function for countrylanguage database variables(country language percentage)*/
 
     public Float getPercentage() {
         return percentage;
     }
-
+    /*Getter and setter function for countrylanguage database variables(country language percentage)*/
     public void setPercentage(Float percentage) {
         this.percentage = percentage;
     }
+    /*Getter and setter function for countrylanguage database variables(country name)*/
     public String getConame() {
         return coname;
     }
-
+    /*Getter and setter function for countrylanguage database variables(country name)*/
     public void setConame(String coname) {
         this.coname = coname;
     }
