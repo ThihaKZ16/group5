49e3e9e4baf176f0706ebef9dbb997865b7653e7
SithuMinZin-40501790 <40501790@live.napier.ac.uk>
2022-04-29 14:24:53 +0630

    update testing

--- src/test/java/com/napier/group5/UnitTesting.java
+++ src/test/java/com/napier/group5/UnitTesting.java
@@ -1,20 +1,128 @@
-//Declare package from com.napier.group5 for unit testing
 package com.napier.group5;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
-/** Create public class for population unit testing */
-public class PopulationUnitTest
-{
-
+public class UnitTesting {
     static App app;
-    /** Connect app.java with the unit testing file */
+    /** connection with app.java */
     @BeforeAll
     static void init()
     {
         app = new App();
-        //app.connect("localhost:33060", 0);
+        app.connect("localhost:33060", 0);
     }
+    /** null value test for display */
+    @Test
+    void displayTestNull1()
+    {
+        app.displaycountry(null);
+    }
+    /** test size for display */
+    @Test
+    void displayTestEmpty1()
+    {
+
+        ArrayList<Country> countries = new ArrayList<Country>();
+        app.displaycountry(countries);
+    }
+
+    /** test if there are null values in display */
+    @Test
+    void displayTestContainsNull1()
+    {
+        ArrayList<Country> countries= new ArrayList<Country>();
+        countries.add(null);
+        app.displaycountry(countries);
+    }
+    /** manual input testing */
+    @Test
+    void displaycountry()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        Country c = new Country();
+        c.setName( "Seoul");
+        c.setCode("KOR");
+        c.setContinent("Asia");
+        c.setRegion("Eastern Asia");
+        c.setConame("South Korea");
+        c.setPopulation(9981619);
+        countries.add(c);
+        app.displaycountry(countries);
+    }
+    /** Test Null value for displaycity */
+    @Test
+    void displayTestNull2()
+    {
+        app.display(null);
+    }
+    /** Test size for displaycity */
+    @Test
+    void displayTestEmpty2()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        app.display(cities);
+    }
+    /** Test if displaycity contains null value */
+    @Test
+    void displayTestContainsNull2()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities.add(null);
+        app.display(cities);
+    }
+    /** Test display city */
+    @Test
+    void display2()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        City ci = new City();
+        ci.setName("Seoul");
+        ci.setCountryCode("KOR");
+        ci.setDistrict("Seoul");
+        ci.setPopulation(9981619);
+        ci.setContinent("Asia");
+        ci.setRegion("Eastern Asia");
+        ci.setConame("South Korea");
+        cities.add(ci);
+        app.display(cities);
+    }
+    /** Test Null value for displaycapital */
+    @Test
+    void displayTestNull3()
+    {
+        app.display(null);
+    }
+    /** Test size for displaycapital */
+    @Test
+    void displayTestEmpty3()
+    {
+        ArrayList<Country> capitalreport = new ArrayList<Country>();
+        app.displaycapital(capitalreport);
+    }
+    /** Test if displaycapital contains null value */
+    @Test
+    void displayTestContainsNull3()
+    {
+        ArrayList<Country> capitalreport = new ArrayList<Country>();
+        capitalreport.add(null);
+        app.displaycapital(capitalreport);
+    }
+    /** Test display capitalcity */
+    @Test
+    void display3()
+    {
+        ArrayList<Country> capitalreport = new ArrayList<Country>();
+        Country cap = new Country();
+        cap.setCode("2413");
+        cap.setConame("Cuba");
+        cap.setContinent("North America");
+        cap.setRegion( "Caribbean");
+        cap.setName("La Habana");
+        cap.setPopulation( 2256000);
+        capitalreport.add(cap);
+        app.displaycapital(capitalreport);
+    }
+
     /** Test Null value for displaypopulation1 */
     @Test
     void displaypopulation1TestNull()
